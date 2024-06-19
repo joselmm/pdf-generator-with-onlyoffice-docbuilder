ba740676b2a737d8dbc672c62bffb96520d3891a
unknown <josealfredosoraca@gmail.com>
2024-06-19 13:53:08 -0500

    algunos ajustes

--- app.js
+++ app.js
@@ -8,15 +8,18 @@ const async = require('async');
 const docbuilderBinaryPath = "C:\\Program Files\\ONLYOFFICE\\DocumentBuilder\\docbuilder.exe"
 const createCvsTemplatePath = __dirname + "\\docbuilder-template-scripts\\crearcsv.docbuilder"
 const createPdfTemplatePath = __dirname + "\\docbuilder-template-scripts\\remplazar.docbuilder"
-const pathToDocxTemplate = __dirname + "\\databasesandtemplate\\" + "Carta ejemplo.docx"
-const dataBasePath = __dirname + "\\databasesandtemplate\\basedatos.xlsx";
+//const docxTemplatePath = __dirname + "\\databasesandtemplate\\" + "Carta ejemplo.docx"
+const docxTemplatePath = "C:\\Users\\SORACA\\Desktop\\Plantilla Diploma - Pt 3.4 Guia 14 - para combinar.docx"
+//const dataBasePath = __dirname + "\\databasesandtemplate\\basedatos.xlsx";
+const dataBasePath = "C:\\Users\\SORACA\\Desktop\\base.xlsx";
 const csvPathName = __dirname + "\\databasesandtemplate\\" + "basedatos.csv"
 const pdfsPath = __dirname + "\\generated-pdfs\\";
 const docbuilderScriptsPath = __dirname + "\\doc-builder-scripts\\"
 const extractDataScriptName = "createCsv.docbuilder";
 const replaceScriptName = "temporal.docbuilder";
-const uniqueProperty = "Nombre";
-const concurrencia = 10;
+const uniqueProperty = "Cédula";
+const concurrencia = 3;
+const fileExtension = 'pdf';
 (async function () {
     try {
         console.log("Hora inicio: "+new Date().toString())
@@ -33,7 +36,7 @@ const concurrencia = 10;
   
       // LEER CONTENIDO DE CREADOR DE PDF
       let createPdfScriptContent = fs.readFileSync(createPdfTemplatePath, 'utf8');
-      createPdfScriptContent = createPdfScriptContent.replaceAll("{{pathToDocxTemplate}}", pathToDocxTemplate);
+      createPdfScriptContent = createPdfScriptContent.replaceAll("{{pathToDocxTemplate}}", docxTemplatePath);
       var textToBeReplaced = "****";
   
       // ITERAR SOBRE CSV
@@ -46,7 +49,8 @@ const concurrencia = 10;
       
       //console.log(csvData.data);
       var eee='oDocument.SearchAndReplace({searchString:"@ENCABEZADO@",replaceString:"@REMPLAZO@"})'
-      var filas = csvData.data;
+     /*  var filas = csvData.data;
+      console.log(filas) */
       /* filas.forEach(async (rowObject) =>{
         //var textoTemporal = createPdfScriptContent.replaceAll("{{pdfName}}", pdfsPath + rowObject.Nombre +".pdf");
         var textoTemporal = createPdfScriptContent;
@@ -64,7 +68,8 @@ const concurrencia = 10;
         await execPromise(commandToPdf);
         console.log(textoTemporal)
     }) */
-        console.log("Hora inicio creaciones pdf: "+new Date().toString())
+        var startTime =  new Date();
+        console.log("Hora inicio creaciones pdf: "+startTime.toString())
         var ordenes = [];
         for (const rowObject of csvData.data) {
             var object = {comando:"",scriptPath:""};
@@ -74,21 +79,25 @@ const concurrencia = 10;
               lineEncabezado = lineEncabezado.replace("@REMPLAZO@", rowObject[key]);
               textoTemporal = textoTemporal.replace(textToBeReplaced, lineEncabezado);
             }
+
             const scriptPath = docbuilderScriptsPath + rowObject[uniqueProperty] +".docbuilder";
-            const pdfPath = pdfsPath + rowObject[uniqueProperty]+ ".pdf";
+            const pdfPath = pdfsPath + rowObject[uniqueProperty]+ "."+fileExtension;
             textoTemporal = textoTemporal.replace(textToBeReplaced, "");
+            //{{fileExtension}}
+            textoTemporal = textoTemporal.replace('{{fileExtension}}',fileExtension);
             textoTemporal = textoTemporal.replace("{{pathToPdfs}}", pdfPath);
             fs.writeFileSync(scriptPath, textoTemporal);
+            console.log(textoTemporal)
             const commandToPdf = `"${docbuilderBinaryPath}" "${scriptPath}"`;
             /* const execPromise = util.promisify(childProcess.exec);
             await execPromise(commandToPdf); */
             object.comando=commandToPdf;
             object.scriptPath=scriptPath;
-            object.nombre=rowObject.Nombre;
+            object[uniqueProperty]=rowObject[uniqueProperty];
            // console.log(textoTemporal);
            ordenes.push(object);
           }
-         // console.log(ordenes)
+          //console.log(ordenes)
 
           /* for (let i = 0; i < ordenes.length; i++) {
             const object = ordenes[i];
@@ -99,24 +108,25 @@ const concurrencia = 10;
           } */
           
         const queue = async.queue(function(task, callback) {
-          console.log("se esta ejecutando el: "+task.nombre)
+          console.log("se esta ejecutando el: "+task[uniqueProperty])
+          //console.log(task)
           const execPromise = util.promisify(childProcess.exec);
           execPromise(task.comando).then(() => {
-            console.log(new Date().toString() + " - finalizo " + task.scriptPath);
-            fs.unlink(task.scriptPath, (err) => {
-              if (err) {
-                console.error(err);
-              } else {
-                console.log(`Script eliminado: ${task.scriptPath}`);
-              }
-            });
+            //console.log(new Date().toString() + " - finalizo " + task.scriptPath);
+            
             callback();
           }).catch((err) => {
             console.error(err);
             callback();
           });
         }, concurrencia);
-        
+        /* fs.unlink(task.scriptPath, (err) => {
+              if (err) {
+                console.error(err);
+              } else {
+                console.log(`Script eliminado: ${task.scriptPath}`);
+              }
+            }); */
         // Agregamos las tareas a la cola
         ordenes.forEach((object) => {
           queue.push(object);
